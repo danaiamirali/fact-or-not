8c49788c2b3d39ddcff49a1c7a0940972cfbe171
yatinbichala <bichalay@umich.edu>
2024-04-07 19:46:41 -0400

    initial example

---
+++ example.py
@@ -0,0 +1,8 @@
+from article_to_text import *
+
+api_key = "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"
+article = "article1.txt"
+
+output = Bias_Detection(article, api_key)
+
+print(output.master_output(output.api_key, output.txt_file))
